2215b85ce92472fe15b784b8744180f0cd1635d5
HitakaK <renkagamine0703@icloud.com>
2023-12-23 03:17:58 +0900

    test0mi2-2

--- test0mi/test0mi2.js
+++ test0mi/test0mi2.js
@@ -1,21 +1,17 @@
-var menu_button = document.getElementById("menu_button");
-var menu_window = document.getElementById("menu_window");
 
-function open_menu(){
-    if(menu_window.style.display === "none"){
-        menu_window.style.display = "block";
-    }
-    else{
-        menu_window.style.display = "none"
-    }
-}
 
-menu_button.addEventListener("click", function(){
-    console.log("func!")
-    if(menu_window.style.display === "none"){
-        menu_window.style.display = "block";
-    }
-    else{
-        menu_window.style.display = "none"
-    }
+
+document.addEventListener("DOMContentLoaded", function(){
+    var menu_button = document.getElementById("menu_button");
+    var menu_window = document.getElementById("menu_window");
+    menu_button.addEventListener("click", function(){
+        console.log("func!");
+        if(menu_window.style.display === "none"){
+            menu_window.style.display = "block";
+        }
+        else{
+            menu_window.style.display = "none"
+        }
+    });
 });
+
