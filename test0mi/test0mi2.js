f5c02be24f87b323fb8a302add4841a970a2157d
HitakaK <renkagamine0703@icloud.com>
2023-12-23 13:22:13 +0900

    test0mi2-2

--- test0mi/test0mi2.js
+++ test0mi/test0mi2.js
@@ -2,16 +2,39 @@
 
 
 document.addEventListener("DOMContentLoaded", function(){
-    var menu_button = document.getElementById("menu_button");
-    var menu_window = document.getElementById("menu_window");
+    let menu_button = document.getElementById("menu_open_button");
+    let menu_window = document.getElementById("menu_window");
+    let menu_button_text = document.getElementsByClassName('menu_button_text');
+    let defalt_window = document.getElementById("body");
     menu_button.addEventListener("click", function(){
         console.log("func!");
-        if(menu_window.style.display === "none"){
+        if(menu_button.id === "menu_open_button"){
             menu_window.style.display = "block";
+            defalt_window.style.display = "none";
+            menu_button.id = "menu_close_button";
+            console.log("funcだね!");
+            if(menu_button_text[0].style.display === "block"){
+                menu_button_text[0].style.display = "none";
+                menu_button_text[1].style.display = "block"
+            }
+            else{
+                menu_button_text[0].style.display = "block"
+                menu_button_text[1].style.display = "none"
+            }
         }
         else{
-            menu_window.style.display = "none"
+            menu_window.style.display = "none";
+            defalt_window.style.display = "block";
+            menu_button.id = "menu_open_button";
+            if(menu_button_text[0].style.display === "block"){
+                menu_button_text[0].style.display = "none";
+                menu_button_text[1].style.display = "block"
+            }
+            else{
+                menu_button_text[0].style.display = "block"
+                menu_button_text[1].style.display = "none"
+            }
         }
     });
+    
 });
-
